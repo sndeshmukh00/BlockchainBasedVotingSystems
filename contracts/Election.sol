9da6b14654057403351968eae2a043ce4ee35941
Shesh Narayan Deshmukh <42098111+sndeshmukh00@users.noreply.github.com>
2021-06-10 21:26:47 +0530

    1. Smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.5.0 <0.8.0;
+
+contract Election{
+    //store candidate
+    //read candidate
+    string public candidate;
+    //constructor
+    constructor() public{
+        candidate = "Modi Ji";
+    }
+}
