24c0d08b045f6d82fc41026db58d442b35ad9fb7
Shesh Narayan Deshmukh <42098111+sndeshmukh00@users.noreply.github.com>
2021-07-10 15:43:11 +0530

    git commit -m "add almost all functions"

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,20 @@
 pragma solidity ^0.5.16;
+
 contract Election{
     //Model a candidate
+
+    // address public admin;
+    // uint public startTime;
+    // uint public endTime;
+
     struct Candidate{
         uint id; //uint = unsigned integer
         string name;
         uint voteCount;
     }
+
+    //store accounts that have voted
+    mapping(address => bool) public voters;
 
     //Store candidates
     //fetch candidates
@@ -15,12 +24,34 @@ contract Election{
 
     //constructor  -->
     constructor() public{
-        addCandidate("Modi Ji");
-        addCandidate("Pappu");
+        // admin = msg.sender;
+        // startTime = now;
+        // endTime = startTime + 5 minutes;
+        addCandidate("Jerry");
+        addCandidate("Shinchan");
+        addCandidate("Tom");
     }
 
     function addCandidate (string memory _name) private{
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
+
+    function vote(uint _candidateId) public {
+        //require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require( _candidateId > 0 && _candidateId <= candidatesCount );
+
+
+        //record that voter has voted
+        voters[msg.sender] = true;
+
+        //update candidate vote count
+        candidates[_candidateId].voteCount ++;
+    }
+
+    
+
 }
--- src/js/app.js
+++ src/js/app.js
@@ -31,9 +31,12 @@ App = {
     var electionInstance;
     var loader = $('#loader');
     var content = $('#content');
+    var voteContent = $('#voteContent');
+    var voteCasted = $('#voteCasted');
 
     loader.show();
     content.hide();
+    voteContent.hide();
 
     web3.eth.getCoinbase(function(err, account){
       if(err === null){
@@ -49,31 +52,113 @@ App = {
       var candidatesResult = $("#candidatesResults");
       candidatesResult.empty();
 
+
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
+      var candidateWon = $('#candidateWon');
+      candidateWon.empty();
+
+
       for(var i = 1; i <= candidatesCount; i++){
         electionInstance.candidates(i).then(function(candidates){
           var id = candidates[0];
           var name = candidates[1];
           var voteCount = candidates[2];
+          
 
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
           candidatesResult.append(candidateTemplate);
+
+          
+
+          var candidateOption = "<option value='" + id + "' >" + name + "</option>";
+          candidatesSelect.append(candidateOption);
+
         });
       }
+      
+      
 
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted){
+      if(hasVoted){
+        $('form').hide();
+        showCountDown();
+        // content.show();
+        voteCasted.show();
+        
+      }
       loader.hide();
-      content.show();
+      // content.show();
+      voteContent.show();
+      
     }).catch(function(error){
       console.warn(error);
     });
 
 
 
-  }
+  },
 
-  
+  castVote: function(){
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance){
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result){
+      $('#content').hide();
+      $('#voteContent').hide();
+      $('#loader').show();
+      
+      location.reload();
+    }).catch(function(err){
+      console.error(err);
+    });
+  }
 
 
 };
+
+function showCountDown(endTime) {
+  var now = new Date().getTime();
+  console.log(now);
+  var timed = now + 30000;
+  var temp = new Date(timed);
+  // console.log(temp);
+  var dateee = temp.toLocaleTimeString();
+  console.log(dateee);
+  var countDownDate = new Date(endTime * 1000);
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    while(timed>now){
+      $('#countDown').text('Election will end at ' + dateee);
+      now = new Date().getTime;
+    }
+    if(now> timed){
+      $('#voteCasted').hide();
+      $('#content').show();
+      $('#countDown').hide();
+      $('#accountAddress').hide();
+      // location.reload();
+    }
+
+    if (distance < 0) {
+      clearInterval(x);
+      $('#countDown').text('Time is up');
+      content.show();
+      location.reload();
+    }
+  }, 1000);
+}
+
 
 $(function() {
   $(window).load(function() {
